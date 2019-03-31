f80bcbb9f85cbf17e0012d04a623f2ba46f81ea8
pxj <271501722@qq.com>
2019-03-31 22:53:50 +0800

    配置过滤器登录验证

--- eladmin-system/src/main/java/me/admin/config/ConfigurerAdapter.java
+++ eladmin-system/src/main/java/me/admin/config/ConfigurerAdapter.java
@@ -1,4 +1,4 @@
-package me.zhengjie.config;
+package me.admin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
