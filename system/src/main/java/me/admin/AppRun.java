d473a2f3ab20b23729003457b86f7fd76b110aa4
pxj <271501722@qq.com>
2019-04-11 21:21:47 +0800

    配置修改和邮件

---
+++ system/src/main/java/me/admin/AppRun.java
@@ -0,0 +1,26 @@
+package me.admin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+/**
+ * @author jie
+ * @date 2018/11/15 9:20:19
+ */
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableWebSocketMessageBroker
+public class AppRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRun.class, args);
+    }
+
+   /* @Bean
+    public SpringContextHolder springContextHolder() {
+        return new SpringContextHolder();
+    }*/
+}
